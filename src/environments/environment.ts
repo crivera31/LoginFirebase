ed44e48262660bbb57016948974c9398de42937d
Diego Alonso Corales Rivera <corales3112@hotmail.com>
2021-05-16 22:39:08 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDfwiMYCSkshreSceN2qakvyhmaAWNm4oc",
-    authDomain: "login-firebase-b12f4.firebaseapp.com",
-    projectId: "login-firebase-b12f4",
-    storageBucket: "login-firebase-b12f4.appspot.com",
-    messagingSenderId: "285560688326",
-    appId: "1:285560688326:web:3dff896a29359935c0d1c7"
+    //PONER DE SU CUENTA FIREBASE
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
